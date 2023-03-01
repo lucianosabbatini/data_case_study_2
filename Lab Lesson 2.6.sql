57a3d1a0f1d5727c3e071d4a09cf22eb924484cf
Luciano <luciano.sabbatini78@gmail.com>
2023-03-01 19:15:47 +0100

    Lab SQL Lesson 2.6

--- Lab Lesson 2.6.sql
+++ Lab Lesson 2.6.sql
@@ -14,6 +14,41 @@ GROUP BY last_name
 HAVING COUNT(*)>1;
 
 #3. 
-SELECT COUNT( DISTINCT staff_id) 
-FROM sakila.rental;
+SELECT staff_id, COUNT(*) as 'Rental per Staff' 
+FROM sakila.rental
+WHERE staff_id IN (1, 2) 
 GROUP BY staff_id;
+
+#4.
+SELECT * FROM sakila.film;
+SELECT COUNT(release_year) FROM sakila.film; # all movies where released in 2006
+
+#5.
+SELECT rating, COUNT(*) AS 'Movies per Rating' 
+FROM sakila.film
+WHERE rating IN ('PG', 'G', 'NC-17', 'PG-13', 'R')
+GROUP BY rating;
+
+#6.
+SELECT rating, ROUND(AVG(length), 2) AS 'Mean Movie Length' 
+FROM sakila.film
+WHERE rating IN ('PG', 'G', 'NC-17', 'PG-13', 'R')
+GROUP BY rating;
+
+#7.
+SELECT rating, ROUND(AVG(length), 2) AS 'Mean Movie Length' 
+FROM sakila.film
+WHERE rating IN ('PG', 'G', 'NC-17', 'PG-13', 'R')
+GROUP BY rating
+HAVING AVG(length)>120;
+
+#8.
+SELECT title, length,
+  CASE 
+    WHEN length>150 THEN 'Super Long Movie'
+    WHEN length BETWEEN 150 and 110 THEN 'Long Movie'
+    WHEN length BETWEEN 60 AND 110 THEN 'Standard Movie' 
+    ELSE 'Short Movie'
+  END AS 'Duration Rank'
+  FROM sakila.film
+  ORDER BY length ASC;
